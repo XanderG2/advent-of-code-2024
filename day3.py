c02ba4c5a88a004c7a5c2bf42585700f5a762b15
Xander <XanderG2@users.noreply.github.com>
2024-12-04 17:11:44 +0000

    part 2 day 3

--- day3.py
+++ day3.py
@@ -3,16 +3,50 @@ with open("inputs/day3.txt", "r") as f:
     input = f.read()
 
 check = r"mul\(\d{1,3},\d{1,3}\)"
+allCheck = r"mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)"
 
-commands = re.findall(check, input)
+def partOne():
+    commands = re.findall(check, input)
 
-sum = 0
+    sum = 0
 
-for command in commands:
-    refined = command.replace("mul(", "").replace(")", "").split(",")
-    number1 = int(refined[0])
-    number2 = int(refined[1])
-    product = number1 * number2
-    sum += product
+    for command in commands:
+        refined = command.replace("mul(", "").replace(")", "").split(",")
+        number1 = int(refined[0])
+        number2 = int(refined[1])
+        product = number1 * number2
+        sum += product
 
-print(sum)
+    print(sum)
+
+def partTwo():
+    commands = {match.start(): match.group()  for match in re.finditer(allCheck, input)}
+    runCommands = []
+    
+    enabled = True
+
+    for command in commands.values():
+        if command == "do()":
+            enabled = True
+        elif command == "don't()":
+            enabled = False
+        else:
+            if enabled:
+                runCommands.append(command)
+
+    sum = 0
+
+    for command in runCommands:
+        refined = command.replace("mul(", "").replace(")", "").split(",")
+        number1 = int(refined[0])
+        number2 = int(refined[1])
+        product = number1 * number2
+        sum += product
+    
+    print(sum)
+
+    
+        
+
+#partOne()
+partTwo()

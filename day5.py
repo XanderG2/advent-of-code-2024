00af7c3d11fc8772db65aaac7bbd47dbe9e6e8d8
Xander <XanderG2@users.noreply.github.com>
2024-12-06 17:05:48 +0000

    part 2 day 5

--- day5.py
+++ day5.py
@@ -1,3 +1,5 @@
+from collections import defaultdict, deque
+
 with open("inputs/day5.txt", "r") as f:
     input = f.read()
 
@@ -13,7 +15,40 @@ def isCorrect(update):
             return False
     return True
 
+def partOne():
+    correctUpdates = [update for update in updates if isCorrect(update)]
+    answer = sum(update[len(update)//2] for update in correctUpdates)
+    print(answer)
 
-correctUpdates = [update for update in updates if isCorrect(update)]
-answer = sum(update[len(update)//2] for update in correctUpdates)
-print(answer)
+def sort(pages, rules):
+    graph = defaultdict(list)
+    in_degree = defaultdict(int)
+    pages_set = set(pages)
+
+    for rule1, rule2 in rules:
+        if rule1 in pages_set and rule2 in pages_set:
+            graph[rule1].append(rule2)
+            in_degree[rule2] += 1
+            in_degree.setdefault(rule1, 0)
+
+    queue = deque([node for node in pages if in_degree[node] == 0])
+    sorted_pages = []
+
+    while queue:
+        current = queue.popleft()
+        sorted_pages.append(current)
+        for neighbor in graph[current]:
+            in_degree[neighbor] -= 1
+            if in_degree[neighbor] == 0:
+                queue.append(neighbor)
+
+    return sorted_pages if len(sorted_pages) == len(pages) else []
+
+def partTwo():
+    incorrect_updates = [update for update in updates if not isCorrect(update)]
+    corrected_updates = [sort(update, rules) for update in incorrect_updates]
+    middle_pages = [update[len(update) // 2] for update in corrected_updates]
+    print(sum(middle_pages))
+            
+partOne()
+partTwo()

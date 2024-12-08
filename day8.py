21390951a4228ca179b6f853cd04971273cc0836
Xander <XanderG2@users.noreply.github.com>
2024-12-08 11:21:07 +0000

    part 2 day 8

--- day8.py
+++ day8.py
@@ -2,7 +2,6 @@ with open("inputs/day8.txt", "r") as f:
     inputLines = [line.strip() for line in f.readlines()]
 
 letters = {}
-antinodePositions = set()
 
 for y in range(len(inputLines)):
     for x in range(len(inputLines[y])):
@@ -14,20 +13,60 @@ for y in range(len(inputLines)):
         else:
             letters[char] = [(x,y)]
 
-for letterPositions in letters.values():
-    for posI in range(len(letterPositions)):
-        for posJ in range(len(letterPositions)):
-            if posI == posJ:
-                continue
-            pos1 = letterPositions[posI]
-            pos2 = letterPositions[posJ]
-            disX = pos2[0] - pos1[0] # right
-            disY = pos2[1] - pos1[1] # down
-            antinodePos1 = (pos1[0] - disX, pos1[1] - disY)
-            antinodePos2 = (pos2[0] + disX, pos2[1] + disY)
-            if 0 <= antinodePos1[0] < len(inputLines[0]) and 0 <= antinodePos1[1] < len(inputLines):
-                antinodePositions.add(antinodePos1)
-            if 0 <= antinodePos2[0] < len(inputLines[0]) and 0 <= antinodePos2[1] < len(inputLines):
-                antinodePositions.add(antinodePos2) 
+def partOne():
+    antinodePositions = set()
 
-print(len(antinodePositions))
+    for letterPositions in letters.values():
+        for posI in range(len(letterPositions)):
+            for posJ in range(len(letterPositions)):
+                if posI == posJ:
+                    continue
+                pos1 = letterPositions[posI]
+                pos2 = letterPositions[posJ]
+                disX = pos2[0] - pos1[0] # right
+                disY = pos2[1] - pos1[1] # down
+                antinodePos1 = (pos1[0] - disX, pos1[1] - disY)
+                antinodePos2 = (pos2[0] + disX, pos2[1] + disY)
+                if 0 <= antinodePos1[0] < len(inputLines[0]) and 0 <= antinodePos1[1] < len(inputLines):
+                    antinodePositions.add(antinodePos1)
+                if 0 <= antinodePos2[0] < len(inputLines[0]) and 0 <= antinodePos2[1] < len(inputLines):
+                    antinodePositions.add(antinodePos2) 
+
+    print(len(antinodePositions))
+
+def partTwo():
+    antinodePositions = set()
+
+    for letterPositions in letters.values():
+        for posI in range(len(letterPositions)):
+            for posJ in range(len(letterPositions)):
+                if posI == posJ:
+                    continue
+                pos1 = letterPositions[posI]
+                pos2 = letterPositions[posJ]
+                disX = pos2[0] - pos1[0] # right
+                disY = pos2[1] - pos1[1] # down
+                allPoses = set()
+                i = 0
+                while True:
+                    bothFalse = 0
+                    antinodePos1 = (pos1[0] - disX*i, pos1[1] - disY*i)
+                    antinodePos2 = (pos2[0] + disX*i, pos2[1] + disY*i)
+                    if 0 <= antinodePos1[0] < len(inputLines[0]) and 0 <= antinodePos1[1] < len(inputLines):
+                        allPoses.add(antinodePos1)
+                    else:
+                        bothFalse += 1
+                    if 0 <= antinodePos2[0] < len(inputLines[0]) and 0 <= antinodePos2[1] < len(inputLines):
+                        allPoses.add(antinodePos2) 
+                    else:
+                        bothFalse += 1
+                    if bothFalse == 2:
+                        break
+                    i += 1
+                for x in allPoses:
+                    antinodePositions.add(x)
+
+    print(len(antinodePositions))
+
+partOne()
+partTwo()

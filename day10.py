321c7144ace12f94b4edf97a69b0d90a75de0d0b
Xander <XanderG2@users.noreply.github.com>
2024-12-14 12:40:07 +0000

    part 2 day 10

--- day10.py
+++ day10.py
@@ -1,6 +1,7 @@
 with open("inputs/day10.txt", "r") as f:
     inputLines = [[int(char) for char in line.strip()] for line in f.readlines()]
 
+DIRECTIONS = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
 def findTrailheads():
     trailheads = []
@@ -10,10 +11,13 @@ def findTrailheads():
                 trailheads.append((x, y))
     return trailheads
 
-def canMove(x, y, prev_height):
-    return 0 <= x < len(inputLines) and 0 <= y < len(inputLines[0]) and inputLines[x][y] == prev_height + 1
+def canMove(x, y, prevHeight):
+    return validPos(x, y) and inputLines[x][y] == prevHeight + 1
 
-def trailhead_score(start):
+def validPos(x, y):
+    return 0 <= x < len(inputLines) and 0 <= y < len(inputLines[0])
+
+def trailheadScore(start):
     visited = set()
     placesToCheck = [start]
     nines = set()
@@ -25,13 +29,37 @@ def trailhead_score(start):
         visited.add((x, y))
         if inputLines[x][y] == 9:
             nines.add((x, y))
-        for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+        for dx, dy in DIRECTIONS:
             x2, y2 = x + dx, y + dy
             if canMove(x2, y2, inputLines[x][y]):
                 placesToCheck.append((x2, y2))
     return len(nines)
 
-trailheads = findTrailheads()
-totalScore = sum(trailhead_score(start) for start in trailheads)
+def countPaths(x, y, height):
+    if not canMove(x, y, height):
+        return False
+    if inputLines[x][y] == 9:
+        return True
+    totalPaths = 0
+    for dx, dy in DIRECTIONS:
+        totalPaths += countPaths(x + dx, y + dy, inputLines[x][y])
 
-print(totalScore)
+    return totalPaths
+
+def findAmountOfPaths(trailhead):
+    x, y = trailhead
+    return countPaths(x, y, -1)
+
+def partOne():
+    trailheads = findTrailheads()
+    totalScore = sum(trailheadScore(start) for start in trailheads)
+
+    print(totalScore)
+
+def partTwo():
+    trailheads = findTrailheads()
+    totalRating = sum(findAmountOfPaths(start) for start in trailheads)
+    print(totalRating)
+
+partOne()
+partTwo()
